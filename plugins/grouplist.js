770bc93e049ce34c9fc3e40f41d1e5f1fbf21c29
tioxand33 <76216550+tioxand33@users.noreply.github.com>
2021-01-23 15:06:11 +0000

    Update grouplist.js

--- plugins/grouplist.js
+++ plugins/grouplist.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
   let txt = conn.chats.array.filter(v => v.jid.endsWith('g.us')).map(v =>`${conn.getName(v.jid)}\n${v.jid} [${v.read_only ? 'Left' : 'Joined'}]`).join`\n\n`
-  conn.reply(m.chat, 'List Groups:\n' + txt, m)
+  conn.reply(m.chat, 'Lista dos grupos q o bot está:\n' + txt, m)
 }
 handler.help = ['groups', 'grouplist']
 handler.tags = ['info']
